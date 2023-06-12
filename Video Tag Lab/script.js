f543d6724c722378268c60452527346ddeb68e2b
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-06-12 23:19:14 +0530

    Update script.js

--- Video Tag Lab/script.js
+++ Video Tag Lab/script.js
@@ -1,9 +1,10 @@
 // Add click event listener to video element and implement play and pause functionality
- const video = document.getElementById('myVideo')video.addEventListener('click', () =>{
-  if(video.paused()){
+ const video = document.getElementById('myVideo')
+ video.addEventListener('click', () => {
+  if(video.paused){
     video.play();
   }
      else {
-        video.paused();
+        video.pause();
      }
 })
