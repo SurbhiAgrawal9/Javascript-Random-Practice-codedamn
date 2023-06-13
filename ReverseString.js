e397438b4869c89a1ae40b28f6fe44a15b688ab6
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-06-13 20:47:37 +0530

    Create ReverseString.js

---
+++ ReverseString.js
@@ -0,0 +1,11 @@
+//  reverse with js 
+
+let s = "surbhi"
+ 
+// convert string to array 
+console.log(s.split("").reverse()) // ["i", "h", "b", "r", "u", "s"]
+
+
+// convert array to string
+// join() is used 
+console.log(s.split("").reverse().join(","))   // i,h,b,r,u,s     
