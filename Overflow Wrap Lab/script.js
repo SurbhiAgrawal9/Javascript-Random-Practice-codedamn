679607d77d9e7e278cce9bcda2ceb2cb4c1e9249
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-06-15 01:46:51 +0530

    Update script.js

--- Overflow Wrap Lab/script.js
+++ Overflow Wrap Lab/script.js
@@ -1,6 +1,6 @@
 function toggleOverflowWrap(){
     const textElement = document.getElementById('t1')
-    if (window.getComputedStyle(textElement).overflowWrap === 'break-word') {
+    if (window.getComputedStyle(textElement).overflowWrap === 'break-word') { //window.getComputedStyle  is written only for testcase purpose 
         textElement.style.overflowWrap = 'normal'
         } else {
             textElement.style.overflowWrap = 'break-word'
